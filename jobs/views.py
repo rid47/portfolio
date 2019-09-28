f34466abea58e753865bc202b24892fe64cd18b6
rid47 <ridwanmizan@gmail.com>
2019-09-28 12:38:34 +0600

    Home page: showing imaage and text from db

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+
+def home(request):
+
+    jobs = Job.objects.all
+    template = 'jobs/home.html'
+    context ={
+        'jobs': jobs,
+    }
+    return render(request, template, context)
